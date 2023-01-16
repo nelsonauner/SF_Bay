ab24e5ea85835702a9213327120769b9f3fa465a
Andrew Cotter <arcotterr@gmail.com>
2023-01-15 17:34:03 -0800

    Starting to write a streamilt app

---
+++ baytemps.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import streamlit_funcs.baytemps as bt
+
+import pymc as pm
+import arviz as az
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from datetime import datetime as dt
+st.set_page_config(layout = "centered")
+st.write("# San Francisco Bay Water Temperature Explorer")
+
+d = bt.import_data()
+daily_average, da2 = bt.average_daily_data(data = d)
+
+#Creating a year x day matrix of mean temperature values for use in the az.plot_hdi function
+interval_data = pd.DataFrame(
+    daily_average.loc[
+        daily_average.year<=2022,:
+    ].pivot(
+        index = "year",columns = "doy", values = "Mean"
+        )
+)
+#Imputing missing values (average temperature from all other years)
+interval_data = interval_data.fillna(interval_data.mean())
+
+year = st.number_input(
+    label = "Enter a year between 1994 and 2023",
+    min_value = 1994, max_value = 2023, 
+    value = 2022
+)
+
+fig, ax = plt.subplots(figsize = (9,6))
+#Creating the 90% interval shaded region
+az.plot_hdi(
+    x = da2.doy, y = interval_data, 
+    hdi_prob = 0.9, fill_kwargs = {"label": "5th-95th Percentile (1994-2023)"}
+    )
+#Average daily temperatures across all years
+plt.plot(da2.doy, da2.Mean, label = "Average (1994-2023)", linestyle = "--")
+
+for year_trace in daily_average.year.unique():
+    plt.plot(
+        daily_average.loc[daily_average.year == year_trace,"doy"],
+        daily_average.loc[daily_average.year == year_trace,"Mean"], 
+        color = "Grey", alpha = 0.1
+    )
+
+#Average daily temperature from selected year
+plt.plot(
+    daily_average.loc[daily_average.year == year,"doy"],
+    daily_average.loc[daily_average.year == year,"Mean"], 
+    color = "Red", label = (str(year))
+    )
+# #What has been collected so far in 2023
+# plt.plot(
+#     daily_average.loc[daily_average.year == 2023,"doy"],
+#     daily_average.loc[daily_average.year == 2023,"Mean"], 
+#     color = "Purple", label = ("2023 YTD")
+#     )
+ax.set_xlabel("Day Of Year")
+ax.set_ylabel("Average Temperature (\N{DEGREE SIGN}F)")
+ax.legend()
+st.pyplot(fig)
---
+++ streamlit_funcs/baytemps.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+@st.experimental_memo()
+def import_data():
+    """
+    An argument-less function that imports all of the hourly SF Bay water data
+    
+    Returns:
+        d (pd.DataFrame) - A dataframe containing hourly San Francisco Bay water temperatures between 1994 and 2023
+    """
+    
+    d = pd.read_csv("data/water_temp/1994.csv")
+    for year in np.arange(start = 1995, stop = 2023):
+        file = "data/water_temp/"+str(year)+".csv"
+        year_data = pd.read_csv(file)
+        d = pd.concat([d,year_data])
+    
+    #Cleaning up the dataframe
+    d.rename(columns = {d.columns[0]:"date", d.columns[1]:"time", d.columns[2]: "temp"}, inplace = True)
+    d = d.loc[:,["date", "time", "temp"]]
+    d.loc[d.temp == "-","temp"] = np.nan
+    d.temp = d.temp.astype("float")
+    d.reset_index(inplace = True)
+
+    #Formatting year, month, day columns
+    d.date = pd.to_datetime(d.date)
+    d["year"] = d.date.dt.year
+    d["month"] = d.date.dt.month
+    d["day"] = d.date.dt.day
+    d["doy"] = d.date.dt.dayofyear
+
+    return d
+
+@st.experimental_memo
+def average_daily_data(data: pd.DataFrame):
+    """
+    Takes the raw hourly temperature data and summarizes it into daily averages
+
+    Arguments:
+        data (pd.DataFrame): Raw hourly temperature data, the same dataframe returned by import_data
+    
+    Returns:
+        daily_average (pd.DataFrame): A dataframe containing the average temperature for every individual day going back to 1994
+        da2 (pd.DataFrame): A dataframe containing the averate temperature for every day of the year across all years
+    """
+    #Calculating averages for each day for every DOY in the dataset (within each year)
+    daily_average = data.groupby(by = ["year", "month", "day","doy"], as_index = False).agg(Mean = ("temp", np.mean), Sd = ("temp", np.std))
+    daily_average.sort_values(by = ["year","month","doy"], inplace = True)
+    daily_average.reset_index(inplace = True)
+
+    #Daily average across all years
+    da2 = daily_average.groupby(by = ["doy"], as_index = False).agg(Mean = ("Mean", np.mean), Sd = ("Mean", np.std), N = ("Mean", len))
+    da2.sort_values(by = ["doy"], inplace = True)
+    da2.reset_index(inplace = True)
+
+    #5 Warmest and coldest days of the year on average
+    da2.sort_values(by = "Mean", inplace = False)
+
+    return daily_average, da2
